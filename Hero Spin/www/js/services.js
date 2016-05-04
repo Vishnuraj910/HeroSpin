b3c453696e3ac0a22aced3c4606ec5a3ddb63a65
Vishnuraj <vishnuraj910@gmail.com>
2016-05-04 18:00:16 +0400

    Made movie posters with fixed height

--- Hero Spin/www/js/services.js
+++ Hero Spin/www/js/services.js
@@ -2,14 +2,10 @@
 
     var data = {};
     if (window.localStorage.getItem("myHero") != null) {data.myHero = window.localStorage.getItem("myHero");} else {data.myHero = 'none';}
-    data.setHero = function (hro) {
-        
-        data.myHero = hro;
-        
+    data.setHero = function (hro) {        
+        data.myHero = hro;       
         window.localStorage.setItem("myHero", data.myHero);
     }
-
-
     return data;
 
 })
